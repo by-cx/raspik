45d08487b57396fc574a69313d77affc23b87adc
Adam Štrauch <cx@initd.cz>
2019-12-09 01:53:15 +0100

    Configuration structure

---
+++ src/web/encryption.go
@@ -0,0 +1,6 @@
+package main
+
+// GetConfig loads configuration from the system and returns it parsed into the right structure
+func GetConfig(password string) (InstanceConfiguration, error) {
+	return InstanceConfiguration{}, nil
+}
